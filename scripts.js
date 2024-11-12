4a2a84fc0b6beaa62d86dc3a8dd23018f69e570b
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-11-12 18:13:02 -0300

    Create gameBoard function

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const user = "x";
+const com = "o";
+
+const gameBoard = function() {
+  let table = [
+    "", "", "", // 0, 1, 2,
+    "", "", "", // 3, 4, 5,
+    "", "", "", // 6, 7, 8,
+  ]
+
+  const getTable = () => table;
+  const setPosition = (player, position) => {
+    table.splice(position, 1, player)
+  }
+
+  return { getTable, setPosition };
+}
+
+const play = gameBoard();
+
+play.setPosition(user, 0);
+play.setPosition(com, 8);
+console.log(play.getTable());
